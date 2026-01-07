f0c38ab3562cfcba24c6b4a5cf3f373b118bfdb6
Vivekanand <vivekanandsharmaaa@gmail.com>
2026-01-07 14:25:14 +0530

    student managment system project

--- Student-Management-System/src/main/java/com/jsp/managementSystem/dto/request/LoginRequest.java
+++ Student-Management-System/src/main/java/com/jsp/managementSystem/dto/request/LoginRequest.java
@@ -1,10 +1,14 @@
 package com.jsp.managementSystem.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     String email;
     String password;
